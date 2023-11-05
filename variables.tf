d1137acfb8fb653d2a9526c83f4619a6ce326540
felipenatividade <felipecmro@hotmail.com>
2023-11-05 20:44:34 -0300

    update pipeline

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-#variables.tf
-/*
-variable "access_key" {
-  description = "Access key to AWS console"
-}
-
-variable "secret_key" {
-  description = "Secret key to AWS console"
-}
-*/
-
 variable "region" {
   description = "AWS region"
 }
@@ -21,11 +10,9 @@ variable "db_user" {
   type = string
 }
 
-
 variable "db_name" {
   type = string
 }
-
 
 variable "db_password" {
   type = string
